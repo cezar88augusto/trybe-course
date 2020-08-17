50c49a8667774baccdd9401e6735c4b4f8631605
cezar88augusto <cezar88augusto@gmail.com>
2020-08-17 18:49:51 -0300

    Initial commit

---
+++ IntroducaoComandos/primeiroProjeto.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Meu primeiro script em Shell Script"
+echo "Criando um diretório!"
+mkdir DiretorioTeste
+echo "Aguarde"; sleep 1; echo . ;sleep 1; echo . ;sleep 1; echo .
+echo "Tudo foi criado com sucesso!"
---
+++ IntroducaoComandos/terceiroProjeto.sh
@@ -0,0 +1,8 @@
+echo "Idade: "
+read IDADE
+if [ "$IDADE" -ge "16" ]
+then
+echo "Voce pode votar"
+else
+echo "Voce ainda n pode votar"
+fi
