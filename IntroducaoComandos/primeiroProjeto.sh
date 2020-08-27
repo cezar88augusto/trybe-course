657812e62aae8167214e9b0f7ea3fe2afdeee251
cezar88augusto <cezar88augusto@gmail.com>
2020-08-27 20:53:22 -0300

    Atualização de reposiório: deixando apenas conteúdo Trybe (exercícios e projetos)

--- IntroducaoComandos/primeiroProjeto.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Meu primeiro script em Shell Script"
-echo "Criando um diretório!"
-mkdir DiretorioTeste
-echo "Aguarde"; sleep 1; echo . ;sleep 1; echo . ;sleep 1; echo .
-echo "Tudo foi criado com sucesso!"
