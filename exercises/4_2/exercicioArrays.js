e703c25e9a2ff9514bfaf5e4199a859809741f64
cezar88augusto <cezar88augusto@gmail.com>
2020-08-28 20:59:41 -0300

    Exercícios bonus resolvidos

--- exercises/4_2/exercicioArrays.js
+++ exercises/4_2/exercicioArrays.js
@@ -118,7 +118,77 @@ console.log(numbers);
 let numbers = [];
 
 for (let i = 0; i < 25; i += 1) {
-    numbers.push(i/2);
+    numbers.push(i / 2);
 }
 
 console.log(numbers);
+
+//Bônus
+//Para os próximos dois exercícios leia este artigo e tente entender o que está acontencedo no código abaixo:
+
+/* for (let i = 1; i < array.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (array[i] < array[j]) {
+        let position = array[i];
+  
+        array[i] = array[j];
+        array[j] = position;
+      }
+    }
+  } */
+
+//1 - Ordene o array numbers em ordem crescente e imprima seus valores;
+
+let array = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let i = 1; i < array.length; i++) {
+    for (let j = 0; j < i; j++) {
+        if (array[i] < array[j]) {
+            let position = array[i];
+
+            array[i] = array[j];
+            array[j] = position;
+        }
+    }
+}
+
+console.log(array)
+
+//2 - Ordene o array numbers em ordem decrescente e imprima seus valores;
+
+let array = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let i = 1; i < array.length; i++) {
+    for (let j = 0; j < i; j++) {
+        if (array[i] > array[j]) {
+            let position = array[i];
+
+            array[i] = array[j];
+            array[j] = position;
+        }
+    }
+}
+
+console.log(array)
+
+//3 - Agora você irá criar um novo array a partir do array numbers, sem perdê-lo. 
+//Cada valor do novo array deverá ser igual ao valor correspondente no array numbers multiplicado pelo seguinte. 
+//Por exemplo: o primeiro valor do novo array deverá ser 45, pois é a multiplicação de 5 (primeiro valor) e 9 (valor seguinte). Já o segundo valor do novo array deverá ser 27, pois é a multiplicação de 9 (segundo valor) e 3 (valor seguinte), e assim por diante. Caso não haja próximo valor, a multiplicação deverá ser feita por 2. 
+//Faça isso utilizando o for e o método push. O resultado deve ser o array abaixo:
+
+//[45, 27, 57, 1330, 560, 800, 200, 70, 945, 54]
+
+let array = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let novoArray = [];
+
+for (let i = 0; i < array.length; i++) {
+
+    if (array[i+1] === undefined) {
+        novoArray.push(array[i]*2);
+    } else {
+        novoArray[i] = array[i] * array[i + 1];
+    }
+
+}
+
+console.log(novoArray)
