818e255f3df578b45fc78d03eb151f5855f05620
Cézar <cezar88augusto@hotmail.com>
2020-09-18 13:46:40 -0300

    Forms updated

--- BLOCO_6/DIA_1/script.js
+++ BLOCO_6/DIA_1/script.js
@@ -36,19 +36,76 @@ for (let index = 0; index < arrayCities.length; index++) {
 }
 
 let inputs = document.getElementsByTagName("input");
+let nome = "";
+let email = "";
+let cpf = "";
 
-
-
-function checkAll(evt) {
-    if (inputs[0].value.length> 10) {
+//Nome:
+inputs[0].addEventListener('keyup', function (evt) {
+    if (inputs[0].value.length > 30) {
         evt.preventDefault();
-        alert("Nome deve menos que 40 caracteres!")
+        alert("Nome deve ter menos que 30 caracteres!")
+    } else {
+        nome = inputs[0].value;
     }
-    if (inputs[1].value.length> 5) {
+})
+
+//Email:
+inputs[1].addEventListener('keyup', function (evt) {
+    if (inputs[1].value.length > 40) {
         evt.preventDefault();
-        alert("Email excede o número de caracteres")
+        alert("Email deve ter menos que 40 caracteres!")
+    } else {
+        email = inputs[1].value;
     }
 
-}
+})
 
-document.getElementById("send").addEventListener('click', checkAll, false);
+//CPF:
+inputs[2].addEventListener('keyup', function (evt) {
+    if (inputs[2].value.length > 11) {
+        evt.preventDefault();
+        alert("CPF deve ter 11 numeros!")
+    } else {
+        cpf = inputs[2].value;
+    }
+})
+
+//Tipo de Casa:
+
+let type = document.getElementsByName("residencia");
+let typeRisidence = "";
+
+type[0].addEventListener('click', function () {
+    typeRisidence = "Casa";
+})
+
+type[1].addEventListener("click", function () {
+    typeRisidence = "Apartamento";
+})
+
+//Consolidado
+let dados = document.getElementsByClassName("consolidado")[0];
+let send = document.getElementById("send");
+
+send.addEventListener("click", function () {
+    let nomeElement = document.createElement("div");
+    nomeElement.innerText = "Nome: " + nome
+        + "\n Email:" + email
+        + "\n Cpf:" + cpf
+        + "\n Tipo de Residência: " + typeRisidence;
+    dados.appendChild(nomeElement);
+})
+
+//Limpar Consolidado
+
+let clean = document.getElementById("clean");
+
+clean.addEventListener('click', function(){
+    while (dados.firstChild){
+        dados.removeChild(dados.firstChild);
+    }
+});
+
+
+
