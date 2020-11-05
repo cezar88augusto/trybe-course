b9fd8d26ef4b63a8f5f261fb24b257b61f6f1c06
cezar88augusto <cezar88augusto@gmail.com>
2020-11-05 17:59:55 -0300

    First component in React

---
+++ BLOCO_11/DIA_1/first-app/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import FirstComponent from './FirstComponent'
+
+function App() {
+  return <FirstComponent/>;
+}
+
+export default App;
---
+++ BLOCO_11/DIA_1/first-app/src/FirstComponent.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class FirstComponent extends Component{
+    render(){
+        return <div><h1>Cézar</h1><p>Tryber Student</p></div>
+    }
+}
+
+export default FirstComponent;
