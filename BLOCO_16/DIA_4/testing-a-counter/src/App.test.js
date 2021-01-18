6b3c6448f830ac9374eda0657382137543a8d3cc
cezar88augusto <cezar88augusto@gmail.com>
2021-01-18 15:57:38 -0300

    Redux

---
+++ BLOCO_16/DIA_4/testing-a-counter/src/App.test.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Provider } from 'react-redux'
+import { render, cleanup } from '@testing-library/react';
+import App from './App';
+import { createStore, combineReducers } from 'redux';
+import clickReducer from './reducers';
+
+const renderWithRedux = (
+  component,
+  { initialState, store = createStore(combineReducers({ clickReducer }), initialState) } = {}
+) => {
+  return {
+    ...render(<Provider store={store}>{component}</Provider>),
+    store,
+  }
+}
+
+//Para que os testes funcionem, o PROVIDER deve ficar no arquivo index.js ENCAPSULANDO O COMPONENTE APP:
+//Deste jeito:
+
+/* ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store} >
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+); */
+
+describe('testing clicks', () => {
+  beforeEach(cleanup);
+  test('the page should has a button and a text 0', () => {
+    const { queryByText } = renderWithRedux(<App />);
+    const buttonAdicionar = queryByText('Clique aqui');
+
+    expect(buttonAdicionar).toBeInTheDocument();
+    expect(queryByText('0')).toBeInTheDocument();
+  });
+  test('a click in a button should increment the value of clicks', () => {
+    const { queryByText } = renderWithRedux(<App />, { initialState: { clickReducer: { counter: 5 } } });
+
+    expect(queryByText('5')).toBeInTheDocument();
+  });
+});
